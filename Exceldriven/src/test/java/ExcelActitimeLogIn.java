ca47ab649a73b0529317d7bbad2205137907fef2
RavishankarJanghel <ravijanghel@gmail.com>
2023-05-31 17:00:09 +0530

    1st Commit.

---
+++ Exceldriven/src/test/java/ExcelActitimeLogIn.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExcelActitimeLogIn {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) throws InvalidFormatException, IOException {
+		File f = new File("C:\\Users\\RAVISHANKAR\\OneDrive\\Desktop\\Ec_program\\Automation\\data\\info.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(f);
+		XSSFSheet sheet = wb.getSheet("Sheet1");
+		String un = sheet.getRow(2).getCell(2).getStringCellValue();
+		String pwd = sheet.getRow(5).getCell(4).getStringCellValue();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.id("username")).sendKeys(un);
+		driver.findElement(By.name("pwd")).sendKeys(pwd);
+		driver.findElement(By.xpath("//div[text()='Login ']")).click();
+	}
+
+}
